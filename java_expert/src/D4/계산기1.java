224dcdd3695a9107fdfab325cb686a785d9a6830
Choyongil <cyi1207@naver.com>
2021-02-15 22:07:15 +0900

    .

--- java_expert/src/D4/계산기1.java
+++ java_expert/src/D4/계산기1.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class 계산기3 {
+public class 계산기1 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
@@ -24,7 +24,7 @@ public class 계산기3 {
 			Stack<Character> s = new Stack<>();
 			
 			for(char c : str) {
-				if (c != '+' && c!= '*' && c != '(' && c != ')') {
+				if (c != '+') {
 					str2.add(c);
 				}
 				else if(c == '+') {
@@ -33,21 +33,6 @@ public class 계산기3 {
 					}
 					s.add(c);
 				}
-				else if(c == '*') {
-					while( s.size() != 0 && s.peek() == '*') {
-						str2.add(s.pop());
-						}
-					s.add(c);
-				}
-				else if(c == '(') {
-					s.add(c);
-				}
-				else if(c == ')') {
-					while(s.peek() != '(') {
-						str2.add(s.pop());
-					}
-					s.pop();
-				}
 			}
 			while(s.size() != 0) {
 				str2.add(s.pop());
@@ -55,16 +40,11 @@ public class 계산기3 {
 			Stack<Integer> ans = new Stack<>();
 			
 			for(char c : str2) {
-				if (c != '+' && c!= '*') {
+				if (c != '+') {
 					ans.add(c - '0');
 				}
 				else {
-					int a = ans.pop();
-					int b = ans.pop();
-					int d;
-					if( c == '+' ) d = a + b;
-					else d = a*b;
-					ans.add(d);
+					ans.add(ans.pop() + ans.pop());
 				}
 			}
 			
