46e0015da8f79a1ca6b161f5902dfd31fc637ffd
Choyongil <cyi1207@naver.com>
2021-02-15 13:28:27 +0900

    .

---
+++ java_expert/src/acmicpc/Silver3/N과_M5.java
@@ -0,0 +1,56 @@
+package acmicpc.Silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N과_M5 {
+	
+	static int n;
+	static int m;
+	static int[] numbers;
+	static int[] input;
+	static StringBuilder sb;
+	static boolean[] isSelected;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		numbers = new int[m];
+		input = new int[n];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0 ; i < n ; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(input);
+		isSelected = new boolean[n];
+		sb = new StringBuilder();
+		func(0);
+		System.out.println(sb);
+	}
+	
+	public static void func(int cnt) {
+		if (cnt == m) {
+			for(int i : numbers) {
+				sb.append(i + " ");
+			}
+			sb.setLength(sb.length() - 1);
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 0 ; i < n ; i++) {
+			if(isSelected[i]) continue;
+			isSelected[i] = true;
+			numbers[cnt] = input[i];
+			func(cnt + 1);
+			isSelected[i] = false;
+		}
+	}
+}
