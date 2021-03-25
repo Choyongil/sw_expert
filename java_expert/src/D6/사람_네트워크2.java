49f130111c4b02f90d467ff41f144bc24098d4b8
Choyongil <cyi1207@naver.com>
2021-03-25 17:53:45 +0900

    .

--- java_expert/src/D6/사람_네트워크2.java
+++ java_expert/src/D6/사람_네트워크2.java
@@ -4,56 +4,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class 사람_네트워크2 {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1 ; tc <= T ; tc++) {
-			sb.append("#").append(tc).append(" ");
-			
-			st = new StringTokenizer(br.readLine());
-			
-			int n = Integer.parseInt(st.nextToken());
-			
-			int[][] arr = new int[n][n];
-			
-			for(int i = 0 ; i < n ; i++) {
-				for(int j = 0 ; j < n ; j++) {
-					int x = Integer.parseInt(st.nextToken());
-					if(i == j) continue;
-					arr[i][j] = x;
-					arr[i][j] = arr[i][j] == 0? 987654321 : arr[i][j];
-				}
-			}
-			
-			for(int k = 0 ; k < n ; k++) {
-				for(int i = 0 ; i < n ; i++) {
-					if(i == k) continue;
-					for(int j = 0 ; j < n ; j++) {
-						if(i == j || j == k) continue;
-						arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
-					}
-				}
-			}
-
-			int ans = Integer.MAX_VALUE;
-			for(int i = 0 ; i < n ; i++) {
-				int sum = 0;
-				for(int j = 0 ; j < n ; j++) {
-					if(arr[i][j] >= 987654321) break;
-					sum += arr[i][j];
-				}
-				ans = Math.min(ans, sum);
-			}
-			sb.append(ans).append("\n");
-		}
-		sb.setLength(sb.length()-1);
-		System.out.println(sb);
-	}
+ 
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+         
+        int T = Integer.parseInt(br.readLine());
+         
+        for(int tc = 1 ; tc <= T ; tc++) {
+            sb.append("#").append(tc).append(" ");
+             
+            st = new StringTokenizer(br.readLine());
+             
+            int n = Integer.parseInt(st.nextToken());
+             
+            int[][] arr = new int[n][n];
+             
+            for(int i = 0 ; i < n ; i++) {
+                for(int j = 0 ; j < n ; j++) {
+                    int x = Integer.parseInt(st.nextToken());
+                    arr[i][j] = x;
+                    arr[i][j] = arr[i][j] == 0? 987654321 : arr[i][j];
+                }
+            }
+             
+            for(int k = 0 ; k < n ; k++) {
+                for(int i = 0 ; i < n ; i++) {
+                    for(int j = 0 ; j < n ; j++) {
+                        arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                    }
+                }
+            }
+ 
+            int ans = Integer.MAX_VALUE;
+            for(int i = 0 ; i < n ; i++) {
+                int sum = 0;
+                boolean flag = true;
+                for(int j = 0 ; j < n ; j++) {
+                    sum += arr[i][j];
+                    if(ans < sum) {
+                    	flag = false;
+                    	break;
+                    }
+                }
+                if(flag) ans = Math.min(ans, sum);
+            }
+            sb.append(ans).append("\n");
+        }
+        sb.setLength(sb.length()-1);
+        System.out.println(sb);
+    }
 }
