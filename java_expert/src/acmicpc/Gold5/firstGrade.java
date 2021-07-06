d4efa2922693dbc4bd504156de72e86cba835417
조용일 <cyi1207@naver.com>
2021-07-06 15:29:47 +0900

    .

---
+++ java_expert/src/acmicpc/Gold5/firstGrade.java
@@ -0,0 +1,13 @@
+package acmicpc.Gold5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class firstGrade {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+	}
+}
